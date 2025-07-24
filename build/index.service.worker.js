5a5c3f04c9f5239d06463046195abc37a9d932d8
Nick <nick.esselman@gmail.com>
2025-07-24 10:22:15 +0200

    new build

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753257859|216742319';
+const CACHE_VERSION = '1753262902|1143707331';
 /** @type {string} */
 const CACHE_PREFIX = 'chess-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
